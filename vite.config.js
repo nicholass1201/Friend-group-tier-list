3fcf2ab04783751ba6b2b2ced8115d78576128cb
Nicholas Sung <nsung1201@gmail.com>
2025-08-08 23:46:30 -0400

    Fix base path for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  // This line is the important one!
+  base: "/Friend-group-tier-list/",
   plugins: [react()],
-})
+});
